3abf95b6f14030dc8100d2d329c841507fc0f620
Kristjan Rang <mail@rang.ee>
2015-10-05 17:43:36 +0300

    Setup api

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
-	"github.com/shiblon/entrogo/scrabble/index"
+	"github.com/krisrang/gobble/Godeps/_workspace/src/github.com/ant0ine/go-json-rest/rest"
+	"github.com/krisrang/gobble/Godeps/_workspace/src/github.com/shiblon/entrogo/scrabble/index"
 )
 
 var (
-	recognizerFile = "wordswithfriends.mealy"
+	wwfFile       = "wordswithfriends.mealy"
+	scrabbleFile  = "TWL06.mealy"
+	wwfIndex      index.Index
+	scrabbleIndex index.Index
 )
 
-func TileCounts(available string) (counts map[byte]int) {
+func tileCounts(available string) (counts map[byte]int) {
 	counts = make(map[byte]int)
 	for _, c := range strings.ToUpper(available) {
 		counts[byte(c)]++
@@ -21,7 +25,7 @@ func TileCounts(available string) (counts map[byte]int) {
 	return
 }
 
-func PossibleWords(idx index.Index, available map[byte]int) <-chan string {
+func possibleWords(idx index.Index, available map[byte]int) <-chan string {
 	allowed := index.NewUnanchoredAllowedInfo(
 		[]string{".", ".", ".", ".", ".", ".", "."},
 		[]bool{true, true, true, true, true, true, true},
@@ -39,23 +43,70 @@ func PossibleWords(idx index.Index, available map[byte]int) <-chan string {
 	return out
 }
 
+func Index(w rest.ResponseWriter, r *rest.Request) {
+	rest.NotFound(w, r)
+}
+
+func GetScrabble(w rest.ResponseWriter, r *rest.Request) {
+	tiles := tileCounts(strings.Replace(r.PathParam("tiles"), "+", ".", -1))
+	words := possibleWords(scrabbleIndex, tiles)
+
+	result := make([]string, 0)
+	for v := range words {
+		result = append(result, v)
+	}
+	w.WriteJson(&result)
+}
+
+func GetWords(w rest.ResponseWriter, r *rest.Request) {
+	tiles := tileCounts(strings.Replace(r.PathParam("tiles"), "+", ".", -1))
+	words := possibleWords(wwfIndex, tiles)
+
+	result := make([]string, 0)
+	for v := range words {
+		result = append(result, v)
+	}
+	w.WriteJson(&result)
+}
+
 func main() {
-	rfile, err := os.Open(recognizerFile)
+	wwffile, err := os.Open(wwfFile)
 	if err != nil {
-		log.Fatalf("Failed to open '%v': %v", recognizerFile, err)
-	}
-	idx, err := index.ReadFrom(rfile)
-	if err != nil {
-		log.Fatalf("Failed to read recognizer from '%v': %v", recognizerFile, err)
+		log.Fatalf("Failed to open '%v': %v", wwfFile, err)
 	}
 
-	available := TileCounts("abcdef")
-	allwords := make([]string, 0, 500)
-	for word := range PossibleWords(idx, available) {
-		allwords = append(allwords, word)
+	wwfIndex, err = index.ReadFrom(wwffile)
+	if err != nil {
+		log.Fatalf("Failed to read recognizer from '%v': %v", wwfFile, err)
 	}
 
-	for _, w := range allwords {
-		fmt.Println(w)
+	scrabblefile, err := os.Open(scrabbleFile)
+	if err != nil {
+		log.Fatalf("Failed to open '%v': %v", scrabbleFile, err)
 	}
+
+	scrabbleIndex, err = index.ReadFrom(scrabblefile)
+	if err != nil {
+		log.Fatalf("Failed to read recognizer from '%v': %v", scrabbleFile, err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/", Index),
+		rest.Get("/scrabble/:tiles", GetScrabble),
+		rest.Get("/wwf/:tiles", GetWords),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+
+	log.Println("Listening on " + port)
+	log.Fatal(http.ListenAndServe(":"+port, api.MakeHandler()))
 }
